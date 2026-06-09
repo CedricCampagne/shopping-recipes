8cacef21b2fd67eb78c5fdd4c9b878824e91c728
CedricCampagne <ced.campagne@gmail.com>
2026-06-09 08:20:39 +0200

    Initialisation du projet : structure monorepo avec API Spring Boot et front Angular

---
+++ api/src/test/java/com/cedric/shoppingrecipes/ShoppingRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cedric.shoppingrecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingRecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
