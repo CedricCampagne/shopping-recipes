31cc7212e444ef24a6e3a46ccc0667f116f3055c
CedricCampagne <ced.campagne@gmail.com>
2026-06-15 10:16:33 +0200

    feat(jwt): ajout des méthodes extract + génération du token

---
+++ api/src/main/java/com/cedric/shoppingrecipes/auth/JwtService.java
@@ -0,0 +1,95 @@
+package com.cedric.shoppingrecipes.auth;
+
+import com.cedric.shoppingrecipes.user.User;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import lombok.RequiredArgsConstructor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class JwtService {
+
+    @Value("${application.security.jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${application.security.jwt.expiration}")
+    private long jwtExpiration;
+
+    private Claims extracAllClaims (String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> resolver) {
+        final Claims claims = extracAllClaims(token);
+        return resolver.apply(claims);
+    }
+
+    // username en SpringSecurity = identifiant du user (nous c'est email dans notre token)
+    // extractUsername lit le subject
+    private String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public Long extractUserId(String token) {
+        return extractClaim(token, claims -> claims.get("id", Long.class));
+    }
+
+    public String extractUserPseudo(String token) {
+        return extractClaim(token, claims -> claims.get("username", String.class));
+    }
+
+    private boolean isTokenExpired(String token) {
+        // before(new Date()) est ce que expiration est avant la date actuelle
+        return extractExpiration(token).before(new Date());
+    }
+
+    // Revoir le concept UserDetails ...
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    // 1) Méthode pour générer un token à partir d'un User
+    public String generateToken(User user) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", user.getId());
+        claims.put("username", user.getUsername());
+
+        return buildToken(claims, user.getEmail());
+    }
+
+    // 2) Méthode interne qui construit vraiment le JWT
+    private String buildToken(Map<String, Object> extraClaims, String subject) {
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+}
