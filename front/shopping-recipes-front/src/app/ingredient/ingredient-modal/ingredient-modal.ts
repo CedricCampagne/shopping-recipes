a4dfd672e8b15097384339e9c6e2af467e539836
CedricCampagne <ced.campagne@gmail.com>
2026-07-15 15:44:07 +0200

    feat(recipe-form): ajout du modal de création d’ingrédient + sélection automatique
    - Ajout du composant IngredientModal (standalone)
    - Ouverture/fermeture du modal via signal dans RecipeForm
    - Création d’un ingrédient directement depuis la page de création de recette
    - Validation complète du formulaire (required, minLength, pattern)
    - Gestion du loader + message de succès homogène avec RecipeForm
    - Emission d’événements (closeModal, ingredientCreated) pour un flux parent propre
    - Ajout automatique du nouvel ingrédient dans la liste locale
    - Sélection automatique du nouvel ingrédient dans le select du RecipeForm
    - Mise à jour automatique de l’unité affichée
    - Reset de la quantité après sélection
    - Amélioration UX globale : pas besoin d’aller dans le module Ingrédient pour créer un nouvel ingrédient

---
+++ front/shopping-recipes-front/src/app/ingredient/ingredient-modal/ingredient-modal.ts
@@ -0,0 +1,82 @@
+import { Component, EventEmitter, inject, Output, signal } from '@angular/core';
+import { FormGroup, ReactiveFormsModule, FormControl } from '@angular/forms';
+import { IngredientsService } from '../services/ingredients.service';
+import { UIStore } from '../../shared/ui.store';
+import { UiMessages } from "../../shared/ui-messages/ui-messages";
+import { Validators } from '@angular/forms';
+import { CreateIngredientRequest } from '../models/create-ingredient-request';
+
+@Component({
+  selector: 'app-ingredient-modal',
+  standalone: true,
+  imports: [ReactiveFormsModule, UiMessages],
+  templateUrl: './ingredient-modal.html',
+  styleUrl: './ingredient-modal.css',
+})
+export class IngredientModal {
+  private ingredientService = inject(IngredientsService);
+  private ui = inject(UIStore);
+
+  units = signal<string[]>([]);
+
+  ingredientForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.pattern(/^\S.*$/)
+    ]),
+    unit: new FormControl('', [Validators.required])
+  });
+
+  @Output() closeModal = new EventEmitter<void>();
+  @Output() ingredientCreated = new EventEmitter();
+
+  ngOnInit() {
+    this.ingredientService.getUnits().subscribe(units =>{
+      this.units.set(units);
+    });
+  }
+
+  createIngredient(){
+    if(this.ingredientForm.invalid) {
+          this.ingredientForm.markAllAsTouched();
+          return;
+        }
+    
+        const request: CreateIngredientRequest = {
+          name: this.ingredientForm.controls.name.value!,
+          unit: this.ingredientForm.controls.unit.value!
+        }
+    
+        this.ui.startLoading();
+
+        this.ingredientService.createIngredient(request).subscribe({
+          next: (newIngredient) => {
+            setTimeout(()=>{
+              this.ui.stopLoading();
+              this.ui.showSuccess("Ingrédient créé avec succès !");
+
+              setTimeout(()=> {
+                this.ingredientCreated.emit(newIngredient);
+                this.closeModal.emit();
+              },1200);
+
+            }, 800);
+          },
+          error: (err) => {
+            this.ui.stopLoading();
+            if (err.status === 409) {
+              this.ui.showError("Ce nom d'ingrédient existe déjà.");
+              return;
+            }
+            this.ui.showError("Erreur lors de la création de l'ingrédient.");
+            console.error("Erreur lors de la création de l'ingredient", err);
+          }
+        });
+  }
+
+  close(){
+    this.closeModal.emit();
+  }
+
+}
