09c9c59b120d5c1175971fb5ae0a3e6f47da9f5a
CedricCampagne <ced.campagne@gmail.com>
2026-08-10 14:20:45 +0200

    refactor: clean up core and security handling

--- front/shopping-recipes-front/src/app/core/interceptors/auth.interceptor.ts
+++ front/shopping-recipes-front/src/app/core/interceptors/auth.interceptor.ts
@@ -2,11 +2,11 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { catchError, EMPTY, throwError } from 'rxjs';
+import { catchError, throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  console.log('authInterceptor → requête sortante:', req.url);
-
+  const router = inject(Router);
+  
   if (req.url.includes('/auth/login') || req.url.includes('auth/register')) {
     return next(req);
   }
@@ -19,32 +19,17 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
     });
   }
 
-  const router = inject(Router);
-
   return next(req).pipe(
     catchError((err) => {
       const status = err.status ?? err.error?.status ?? err.error?.code ?? err.error?.error;
 
-      //Laisser passer les erreurs métier
-      if (status === 409 || status === 400 || status === 404) {
-        return throwError(() => err);
-      }
-
-      // Auth seulement
       if (status === 401) {
         console.error('401 : session expirée ou token invalide');
         localStorage.removeItem('token');
         router.navigate(['/login']);
-        return throwError(() => err); // surtout PAS EMPTY
-      }
-
-      // NE PAS rediriger sur 403
-      // 403 = erreur métier, pas auth
-      if (status === 403) {
         return throwError(() => err);
       }
 
-      // autres erreurs passent
       return throwError(() => err);
     }),
   );
